885b6c8fc4990838ccd5e9cf091f9b6225876b0d
j23414 <jenchang@iastate.edu>
2023-03-25 07:58:59 -0700

    basic react website

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
